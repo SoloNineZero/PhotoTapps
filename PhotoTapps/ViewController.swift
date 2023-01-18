da3f488d72c71ed488dae7013aef3ec409b93ba9
SoloNineZero <solodyankin.ie@ya.ru>
2023-01-18 14:44:10 +0500

    Initial Commit

---
+++ PhotoTapps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoTapps
+//
+//  Created by Игорь Солодянкин on 18.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
